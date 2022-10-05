75682a502e6f5d3d5026cd8ee68a57b596eb624d
Lukas Allmer <lukas@allmer.org>
2022-10-05 18:21:15 +0200

    automatic command deployment on bot start

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,26 +1,29 @@
-const fs = require('node:fs');
-const path = require('node:path');
-const { REST, Routes } = require('discord.js');
-const { clientId, token } = require('./config.json');
+function deployCommands() {
+	const fs = require('node:fs');
+	const path = require('node:path');
+	const { REST, Routes } = require('discord.js');
+	const { clientId, token } = require('./config.json');
 
-// This fetches all .js files from the commands folder and adds it to the commands!
-const commands = [];
-const commandsPath = path.join(__dirname, 'commands');
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	// This fetches all .js files from the commands folder and adds it to the commands!
+	const commands = [];
+	const commandsPath = path.join(__dirname, 'commands');
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
-for (const file of commandFiles) {
-	const filePath = path.join(commandsPath, file);
-	const command = require(filePath);
-	commands.push(command.data.toJSON());
-}
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		commands.push(command.data.toJSON());
+	}
 
-const rest = new REST({ version: '10' }).setToken(token);
+	const rest = new REST({ version: '10' }).setToken(token);
 
-// Enable to deploy commands globally
-rest.put(Routes.applicationCommands(clientId), { body: commands })
-	.then((data) => console.log(`Successfully registered ${data.length} application commands.`))
-	.catch(console.error);
-    
+	// Enable to deploy commands globally
+	rest.put(Routes.applicationCommands(clientId), { body: commands })
+		.then((data) => console.log(`Successfully registered ${data.length} application commands.`))
+		.catch(console.error);
+};
+
+module.exports = {deployCommands};
 
 // Enable to delete all global commands
 /* rest.put(Routes.applicationCommands(clientId), { body: commands })
--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, GatewayIntentBits } = require('discord.js');
 const {token} = require('./config.json');
+const deploy = require("./deploy-commands.js");
+deploy.deployCommands();
 
 // Intents are required for access to events
 const client = new Client({
