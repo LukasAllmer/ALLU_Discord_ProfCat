697882d4a119059920d4c9ac9831979d189ea544
Lukas Allmer <lukas@allmer.org>
2022-10-05 22:54:17 +0200

    fix again lol

--- commands/createPoll.js
+++ commands/createPoll.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder } = require('discord.js');
-const emojiCharacters = require('./emojicharacters.js');
+const emojiCharacters = require('emojicharacters.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
