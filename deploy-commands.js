69c7f15acc8868383ca794c2d2f6878ccf753993
Lukas Allmer <lukas@allmer.org>
2022-10-05 17:26:39 +0200

    Second initial commit - publishes actual code lol

---
+++ deploy-commands.js
@@ -0,0 +1,32 @@
+const fs = require('node:fs');
+const path = require('node:path');
+const { REST, SlashCommandBuilder, Routes } = require('discord.js');
+const { clientId, token } = require('./config.json');
+
+// This fetches all .js files from the commands folder and adds it to the commands!
+const commands = [];
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	commands.push(command.data.toJSON());
+}
+
+const rest = new REST({ version: '10' }).setToken(token);
+
+// Enable to deploy commands globally
+rest.put(Routes.applicationCommands(clientId), { body: commands })
+	.then((data) => console.log(`Successfully registered ${data.length} application commands.`))
+	.catch(console.error);
+    
+
+// Enable to delete all global commands
+/* rest.put(Routes.applicationCommands(clientId), { body: commands })
+    .then((data) => console.log(`Successfully registered ${data.length} application commands.`))
+    .catch(console.error);    */
+// Enable to delete all commands from a certain guild
+/* rest.put(Routes.applicationGuildCommands(clientId, 'guildId'), { body: [] })
+.then(() => console.log('Successfully deleted all guild commands.'))
+.catch(console.error); */
---
+++ main.js
@@ -0,0 +1,80 @@
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, GatewayIntentBits } = require('discord.js');
+const {token} = require('./config.json');
+
+// Intents are required for access to events
+const client = new Client({
+    intents: [
+        GatewayIntentBits.DirectMessageReactions,
+        GatewayIntentBits.DirectMessageTyping,
+        GatewayIntentBits.DirectMessages,
+        GatewayIntentBits.GuildBans,
+        GatewayIntentBits.GuildEmojisAndStickers,
+        GatewayIntentBits.GuildIntegrations,
+        GatewayIntentBits.GuildInvites,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildMessageReactions,
+        GatewayIntentBits.GuildMessageTyping,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildPresences,
+        GatewayIntentBits.GuildScheduledEvents,
+        GatewayIntentBits.GuildVoiceStates,
+        GatewayIntentBits.GuildWebhooks,
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.MessageContent]
+});
+
+client.commands = new Collection();
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	// Set a new item in the Collection
+	// With the key as the command name and the value as the exported module
+	client.commands.set(command.data.name, command);
+}
+
+client.once('ready', () => {
+    console.log("ProfCat is ready!");
+})
+
+// Respond to commands from deploy-commands.js
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	} else if (commandName === 'server') {
+		await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
+	} else if (commandName === 'user') {
+		await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
+	}
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Has to be last line in file!
+client.login(token);
