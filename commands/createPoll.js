08d6ad051e75911b4356ed4ca50918cad8dd97b0
Lukas Allmer <lukas@allmer.org>
2022-10-05 22:18:09 +0200

    Testing posting message and reacting!

---
+++ commands/createPoll.js
@@ -0,0 +1,14 @@
+const { SlashCommandBuilder } = require('discord.js');
+const emojiCharacters = require('./emojiCharacters.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('createPoll')
+		.setDescription('Creates a poll.'),
+	async execute(interaction) {
+		await interaction.channel.send('My message.').then(sentMessage => {
+            sentMessage.react(emojiCharacters[0]);
+            sentMessage.react(emojiCharacters[1]);
+        })
+	},
+};
