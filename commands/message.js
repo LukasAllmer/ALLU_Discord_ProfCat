03be249e05592e006fd4ff349771c112afbdefd7
Lukas Allmer <lukas@allmer.org>
2022-10-06 17:14:03 +0200

    added user feedback

--- commands/message.js
+++ commands/message.js
@@ -5,7 +5,10 @@ module.exports = {
 		.setName('message')
 		.setDescription('Sends a message to channel where called!'),
 	async execute(interaction) {
-
+		await interaction.reply({
+			content: 'Understood, I will post a message!', 
+			ephemeral: true
+		});
 		await interaction.channel.send('My message.');
 	},
 };
